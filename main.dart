4a7bb0539f6b0ce359bbd866b129643bfd8b8f18
Hilal N. Tek <53139667+Tek-nr@users.noreply.github.com>
2021-12-29 05:46:02 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,128 +1,222 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
-typedef konuVeEylemFunc = String Function({String eylem, required String konu});
+/*class Ogrenci{
+  String ad;
+  String soyad;
+  int yas;
+  String cinsiyet;
+  int not;
+  bool mezunMu;
+  static String okulAdi = "AliVeli Universitesi";
+
+  Ogrenci(this.ad, this.soyad, this.yas, this.cinsiyet, this.not): mezunMu=false ;
+
+  bool ortalamaninUstundeMi(double ortalama){
+    return not>ortalama;
+  }
+
+  String get adiSoyadi => "$ad $soyad";
+  set adiSoyadi(String value){
+    int bosluk = value.indexOf(" ");
+    ad = value.substring(0,bosluk);
+    soyad = value.substring(bosluk+1);
+  }
+
+  static void setOkulAdi(String yeniOkulAdi){
+    okulAdi = yeniOkulAdi;
+  }
+
+  @override
+  String toString() {
+    return "$ad $soyad $yas $cinsiyet $not $mezunMu";
+  }
+}
+
+String? getAli() => "ali";*/
+
+
+class Ogrenci{
+  String ad;
+  late int yas;
+  Ogrenci(this.ad, this.yas);
+  Ogrenci.OgrenciC2(this.ad, int y ): yas = y;
+  Ogrenci.OgrenciC3(this.ad, int y){
+    yas = y; //-> yas late olmasaydı kabul etmezdi
+  }
+  Ogrenci.onsekiz(String ad): this(ad,18);
+
+  void merabaDe(){
+    print("Merhaba ben $ad, $yas yasindayim.");
+  }
+}
 
 void main() {
+  print("calisiyorr");
+  Ogrenci o1 = Ogrenci.OgrenciC2("Ali", 16);
+  Ogrenci o2 = Ogrenci.onsekiz("Melahat");
+  final Ogrenci o3 = Ogrenci("ali", 19);
+  o3.yas=22;
+  o1.merabaDe();
+  o2.merabaDe();
+  o3.merabaDe();
+  /*String s;
+  s="ali";
+  print(s);
+  print(s.length);
 
-  print("Main calisti");
-  var konu = "Flutter";
-  var eylem = " " + "ogreniyorum";
-  String hepsi = yazdirHepsi(konu, eylem);
-  print(hepsi);
-
-  String hepsi1 = yazdirHepsi(konu);
-  print(hepsi1);
-
-  //var f = konuVeEylem;
-  //String Function({String eylem = '', required String konu}) f = konuVeEylem;
-
-  /*konuVeEylemFunc f = ({ String eylem = '', required String konu}) {
-    String hepsi = konu + eylem;
-    return hepsi;
-  };*/
-
-  konuVeEylemFunc f = ({required String konu, String eylem = ''}) => konu + " "+ eylem;
-
-  String hepsi2= konuVeEylem(konu:konu, eylem:eylem);
-  print(hepsi2);
-  
-  String hepsi3 = f(konu: konu);
-  print(hepsi3);
-
-  var x ="Flutter";
+  String? sn;
+  sn = getAli();
+  print(sn);
+  if(sn !=null){
+    print(sn.length +5);
+  }*/
 
 
-
-/*  for (int i = 0; i<10; i++){
-    print("number $i");
+  /*final ogr1 = Ogrenci("ali", "demir", 21, "-", 56);
+  final ogr2 = Ogrenci("veli", "celik", 22, "-", 86);
+  print("$ogr1 , $ogr2");
+  print("${ogr1.not} - ${ogr2.not}");
+  if(ogr2.ortalamaninUstundeMi(70)){
+    print( "Tebrikler" );
   }
-  var main_tekrar_calisti = "main tekrar" + " calisti";
-  print(main_tekrar_calisti);
-  
-  print( findAr(main_tekrar_calisti));
-  print("main bitmek uzere");
-  //runApp(const MyApp());*/
-}
+  print(ogr1.adiSoyadi);
+  ogr2.adiSoyadi = "Ali Demir Celik";
+  print(ogr2);
+  print(Ogrenci.okulAdi);
+  String? x;
+  print(x?.length);*/
 
-String konuVeEylem({required String konu, String eylem = ''}) {
-  String hepsi = konu + eylem;
-  return hepsi;
-}
+  /*final liste=[
+    {"1","2"},
+    {"3","4"},
+    {"5","6"},
+  ];
+  print(liste);
+  final ikinci = liste[1];
+  ikinci.remove("4");
+  print(ikinci);
+  print(liste);
 
-String yazdirHepsi(String konu, [String eylem = '']) {
-  print("konu ve eylem birleştirilecek");
-  String hepsi = konu + eylem;
-  return hepsi;
-}
+  var liste1= [1,2,3];
+  var liste2= [4,5,6];
 
+  print(liste1);
+  print(liste2);
+  liste2 = liste1;
+  print("sonrasi: ");
+  print(liste2);
+  print(liste1);
 
+  liste2[1]=10;
+  print(liste1);
+  print(liste2);*/
 
-/*int findAr(String s) {
-  var index = s.indexOf('ar');
-  return index;
-}*/
+  /*final etiketler = {
+    "arkiler":1,
+    "okul":2,
+    "kurs":55
+  };
 
-/*class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  print(etiketler);
+  print(etiketler["is"]);
+  print(etiketler["arkiler"]);
+  etiketler["arkiler"]=36;
+  print(etiketler["arkiler"]);
+  print(etiketler.containsKey("okul"));*/
 
-  @override
-  Widget build(BuildContext context) {
-    print("Build calisti");
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  /*final mesajlar = ["a","b","c"];
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  print("Hosgeldiniz");
+  var mesajSayisi = 10;
+  var mi =0;
 
-  final String title;
+  print(mesajlar);
+  mesajlar.add("d");
+  print(mesajlar);
+  mesajlar.addAll(["e","f"]);
+  print(mesajlar);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    print("counter calisti");
-    setState(() {
-      _counter++;
-    });
+  if(mesajlar.contains("a")){
+    print("Mesaj Var!");
+  } else{
+    print("Mesaj Yok!");
   }
 
-  @override
-  Widget build(BuildContext context) {
-    print("myhomepagestate calisti");
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Bro, you have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+  mesajlar.remove("c");
+  print(mesajlar);
+
+  mesajlar.removeAt(2);
+  print(mesajlar);
+
+  print(mesajlar.first);
+  print(mesajlar.last);
+
+  if(mesajlar.isEmpty){
+    print("mesaj yok!");
+  } else{
+    print("mesaj var!");
+  }*/
+
+/*  while(mi<mesajSayisi){
+    print("$mi. mesaj");
+    mi++;
+  }*/
+
+  /*for(int i=0;i<mesajlar.length;i++){
+    print(mesajlar[i]);
   }
+
+  for (var i in mesajlar){
+    print("Mesaj: ");
+    if(i == "b")
+      continue;
+    print(i);
+  }
+
+  print("${mesajlar.length*2} adet okunmamış mesajınız var!");*/
+
+  /*if(mesajSayisi > 0){
+    print("Yeni mesajiniz var!");
+  } else if(mesajSayisi == 1){
+    print("Bir yeni mesajiniz var!");
+  } else{
+    print("Yeni mesajiniz yok!");
+  }
+
+  switch(mesajSayisi){
+    case 1:
+      print("Bir yeni mesaj");
+      break;
+    case 2:
+      print("İki yeni mesaj!");
+      break;
+    case 3:
+      print("Uc yeni mesaj!");
+      break;
+    default:
+      print("Cok yeni mesaj!");
+      break;
+  }
+
+  print(mesajSayisi == 0 ? "Yeni mesaj yok..." : "Yeni mesaj var...");
+
+  print("$mesajSayisi adet okunmamis mesajiniz var!");*/
+/*  final a = "a";
+  final deg = "degisken "+a;
+  var d = "degisken";
+
+  var x;
+  x=1;
+  x="bir";
+  print(x);
+
+  print(deg);
+  print("main calisti");
+  altProgram();*/
+}
+
+
+
+/*void altProgram(){
+  print("alt program calisti");
 }*/
